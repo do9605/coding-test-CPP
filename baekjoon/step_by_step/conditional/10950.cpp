de64c0f1493f86d66f1c2e9fbcd2ebcc9fb50eaa
do9605 <do9605@unist.ac.kr>
2022-05-17 21:52:49 +0900

    https://www.acmicpc.net/problem/10950

---
+++ baekjoon/step_by_step/conditional/10950.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    short val;
+    short* val_ptr;
+    short* val_ptr_array[9];
+    short idx;
+
+    cin >> val;
+    idx = val;
+
+    while (val > 0) {
+        val_ptr = new short[2];
+
+        cin >> val_ptr[0] >> val_ptr[1];
+        // cout << "====" << endl;
+        // cout << idx-val << endl;
+        // cout << "====" << endl;
+        val_ptr_array[idx-val] = val_ptr;
+        val -= 1;
+    }
+
+    while (val != idx) {
+        cout << "====" << endl;
+        cout << val << endl;
+        cout << "====" << endl;
+        cout << val_ptr_array[val][0] + val_ptr_array[val][1] << endl;
+        delete[] val_ptr_array[val];
+        val += 1;
+    }
+    return 0;
+}
